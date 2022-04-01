24bbc7c78edb35a60f2ec44dcf9f8d337ed15802
Bluemarble <hoangledinh65@gmail.com>
2022-04-01 10:22:18 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -78,7 +78,7 @@ resource "aws_instance" "backend" {
 
 resource "aws_key_pair" "dev" {
   key_name   = "dev-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHAzSVTR1P7Iu9OjsfGAId52pefxUku6t9vPNhv1i9P0 ws0599@administrator-OptiPlex-3080"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD/yC1uKSiTgQwAl7GhRPod41Vk1aAcFENp68b7jPJuqmGllRl29tYuI9C77PLqGoC3oiNa11hhED9ChDsOhpPuKaY0jjPjqtDWr8mTeKbJkl17V8uMpsaN+9GrbRZBl6TxSAtlWcjy/+mddnSpXeZClMT2Vb4/o2Xnw0XTa9viU8ImceHOYSsAg8NQSSBBp6JSpghJ+5fWUpGY2DS/586xLcaSfBCJVFdbEiL57HrZ0gjs4QNtosbOoCzqSnyXGLxwmOlLEQRpIH0UsDoA1uWr+i2xDlCVqC6h8YtmlrWRHqEckc8NR1g5zC1ZpT8gV8lZHVK/jnzo1VOsJJcSbxX4rHutcFycbtJvzqFWtxmLGpiM+nlGMuIl7TTitmb67/YerNgRV6DcOJ+dEYYK3ZXws8j1J99SRd4Mee7iyVofvNOxlBSZTCeTexAJ2wrpEQQlzf7i7tEPO4REAxLz/ILApQkucvLJw/ZmPFSM0jz7qVIn90w6IV3BfyUBAqsCG0= admin123@Admin"
 }
 
 # tạo security group allow ssh
@@ -93,7 +93,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["13.213.52.236/32"]
+    cidr_blocks = ["18.136.123.97/32", "14.248.82.236/32"]
   }
 
   egress {
@@ -107,4 +107,8 @@ resource "aws_security_group" "allow_ssh" {
 # in ra public ip của ec2 instance
 output "ec2_instance_public_ips" {
   value = aws_instance.web.*.public_ip
+}
+# in ra public ip của ec2 instance
+output "ec2_instance_public_ips" {
+  value = aws_instance.backend.*.public_ip
 }
